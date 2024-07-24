8e0f98c659086194e8a5ded78301f87532d2b23c
Sherzodjon <124202641+tursunov4@users.noreply.github.com>
2024-07-24 10:17:32 +0500

    dsfasdf

---
+++ src/components/Electype/Electrtype.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+const Electrtype = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="firsttype" />
+      </div>
+      <div>
+        <img src="" alt="secondtype" />
+      </div>
+    </div>
+  );
+};
+
+export default Electrtype;
